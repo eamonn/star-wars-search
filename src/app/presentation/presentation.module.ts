08a506f50e375d10eb7108087c9742e6538f4d17
Eamonn Mooney <eamonn90@gmail.com>
2018-05-22 23:31:04 +0100

    Move Header to presentation module
    .

--- src/app/presentation/presentation.module.ts
+++ src/app/presentation/presentation.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
 import { ResultCardComponent } from './result-card/result-card.component';
 import { ResultCardRowComponent } from './result-card-row/result-card-row.component';
 import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
@@ -9,11 +10,13 @@ import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.compo
     CommonModule
   ],
   declarations: [
+    HeaderComponent,
     ResultCardComponent,
     ResultCardRowComponent,
     LoadingSpinnerComponent
   ],
   exports: [
+    HeaderComponent,
     ResultCardComponent,
     ResultCardRowComponent,
     LoadingSpinnerComponent
