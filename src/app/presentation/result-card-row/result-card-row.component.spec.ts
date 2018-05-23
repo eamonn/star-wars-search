1cbbec56f0d459b6182982ac8c54adc3a8ae5e00
Eamonn Mooney <eamonn90@gmail.com>
2018-05-23 21:28:50 +0100

    Tests for Result Card Row

--- src/app/presentation/result-card-row/result-card-row.component.spec.ts
+++ src/app/presentation/result-card-row/result-card-row.component.spec.ts
@@ -1,25 +1,54 @@
+import { Component } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ResultCardRowComponent } from './result-card-row.component';
 
+@Component({
+  template: `
+    <em-result-card-row [property]="prop">{{content}}</em-result-card-row>`
+})
+class TestHostComponent {
+  content: string = 'mock content';
+  prop: string = 'mock prop';
+}
+
 describe('ResultCardRowComponent', () => {
-  let component: ResultCardRowComponent;
-  let fixture: ComponentFixture<ResultCardRowComponent>;
+  let testHost: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ResultCardRowComponent ]
+      declarations: [ TestHostComponent, ResultCardRowComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ResultCardRowComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(TestHostComponent);
+    testHost = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(testHost).toBeTruthy();
   });
+
+  it('should set property name to foo, render in dom as foo:', () => {
+    testHost.prop = 'foo';
+    fixture.detectChanges();
+
+    const target = fixture.nativeElement.querySelector('.property').innerText;
+
+    expect(target).toEqual('foo:');
+  });
+
+  it('should display the content as "bar"', () => {
+    testHost.content = 'bar';
+    fixture.detectChanges();
+
+    const target = fixture.nativeElement.querySelector('.value').innerText;
+
+    expect(target).toEqual('bar');
+  });
+
 });
